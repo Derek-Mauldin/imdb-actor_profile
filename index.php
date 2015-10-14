3046caf39ad3a9a8a571ba8401b4474d87c29df6
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-10-14 16:58:09 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html lang="en">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Actor Profile</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<p>
+			A dedicated and knowledgeable Movie & Television fan that wants to research and profile actors and their work.
+			He/she is very knowledgeable about many actors and may have something to contribute to the database.
+		</p>
+		<h2>Use Case 1</h2>
+		<p>Goal: Persona is looking for more information on an actor the he/she is unfamiliar with</p>
+		<ul>
+			<li>User will go to the home page and enter the name of the actor in the search</li>
+			<li>User will click on the button that displays the general characteristics of the actor</li>
+			<li>General Bio will be displayed</li>
+		</ul>
+		<h2>Use Case 2</h2>
+		<p>Goal: User is looking to update information on an actor that he/she is very famaliar with</p>
+		<ul>
+			<li>User will go to the home page and enter actors name in the search</li>
+			<li>User will choose either biography or trivia for the actor</li>
+			<li>User will chose the Update Actor Info button</li>
+			<li>User will be presented with a form to fill out with new info on the actor</li>
+			<li>User will enter info into the form</li>
+			<li>User will submit form</li>
+		</ul>
+	</body>
+</html>
