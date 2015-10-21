9e61f482625af24735e4ac89b41e3dd3b21fc8c3
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-10-21 16:01:36 -0600

    added mutator methods setActorId and setActorName

--- php/classes/non-foreign-key-class.php
+++ php/classes/non-foreign-key-class.php
@@ -44,6 +44,38 @@ class actorProfile {
 }
 
 	/**
+	 * Mutator Method for actor ID
+	 *
+	 * @param mixed $newActorId: ID value for a new actor
+	 * @throws InvalidArgumentException if $newActorId is not an integer
+	 * @throws RangeException if $newActorID is not positive
+	 *
+	 **/
+	public function setActorId($newActorId) {
+
+		// base case: if the actorId is null, this is a new actor without a mySQL assigned ID
+		if($newActorId === null) {
+			$this->actorId = null;
+			return;
+		}
+
+		// validate that $newActorId is an integer
+		$newActorId = filter_var($newActorId, FILTER_VALIDATE_INT);
+		if($newActorId === false) {
+			throw(new InvalidArgumentException("Actor ID is not a valid integer"));
+		}
+
+		// validate that $newActorId is positive
+		if($newActorId <= 0) {
+			throw(new RangeException("Actor ID is not a positive number"))
+		}
+
+		// convert and store the actor ID
+		$this->actorId = intval($newActorId);
+
+	}
+
+	/**
  	* Accessor Method for this actors name
  	*
  	* @return string value of actors name
@@ -51,6 +83,52 @@ class actorProfile {
 	public function getActorName() {
 		return ($this->actorName);
 }
+
+	/**
+	 * Mutator Method for actor name
+	 *
+	 * @param mixed $newActorName: ID value for a new actor
+	 * @throws InvalidArgumentException if $newActorName is insecure or empty
+	 * @throws RangeException if $newActorName is too large for the database
+	 *
+	 **/
+	public function setActorName($newActorName) {
+
+		// verify that that $newActorName is secure
+		$newActorName = trim($newActorName);
+		$newActorName = filter_var($newActorName, FILTER_SANITIZE_STRING);
+		if(empty($newActorName) === true) {
+			throw(new InvalidArgumentException("Actor Name is insecure or empty"));
+		}
+
+		// verify the new actor name will fit the database
+		if(strlen($newActorName) > 40) {
+			throw(new RangeException("Actor Name is too large"))
+		}
+
+		// store actor name
+		$this->actorName = $newActorName;
+
+	}
+
+	/**
+	 * Mutator Method for actor name
+	 *
+	 * @param mixed $newActorName: ID value for a new actor
+	 * @throws InvalidArgumentException if $newActorName is insecure or empty
+	 * @throws RangeException if $newActorName is too large for the database
+	 *
+	 **/
+	public function setBirthday($newBirthday) {
+
+		// base case if $newBirthday
+		$newBirthday = trim($newBirthday);
+
+
+
+
+	}
+
 	/**
 	 * Accessor Method for this actors birthday
 	 *
@@ -60,6 +138,7 @@ class actorProfile {
 	public function getBirthday() {
 		return($this->birthday);
 	}
+
 
 	/**
 	 *Accessor Method for this actors birth name
@@ -75,18 +154,10 @@ class actorProfile {
 	 *
 	 * @return string value of actors height
 	 **/
-public function() {
-	return($this->height);
-}
+	public function getHeight() {
+		return($this->height);
+	}
 
-
-} // close class actorProfile
-
-
-
-
-
-
-
+}  // close class actorProfile
 
 ?>
